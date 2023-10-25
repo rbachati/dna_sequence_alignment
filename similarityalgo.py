6fdcaa6b6d9248a888fb14361a916f0b9952321a
Karanjithkaizoku <146391757+Karanjithkaizoku@users.noreply.github.com>
2023-10-24 20:05:59 -0700

    update max continuous chain and display them

--- similarityalgo.py
+++ similarityalgo.py
@@ -1,8 +1,3 @@
-def main_menu():
-    print("1. Set Maximum Shift")
-    print("2. Calculate using File Inputs")
-    print("3. Exit")
-
 def get_user_input(prompt):
     return input(prompt)
 
@@ -10,9 +5,48 @@ def load_sequence_from_file(filename):
     with open(filename, 'r') as file:
         return file.read().strip()
 
+def identify_chained_sequences(sequence1, sequence2):
+    chained_sequences = []
+    chain = ""
+    for a, b in zip(sequence1, sequence2):
+        if a == b:
+            chain += a
+        else:
+            if chain:
+                chained_sequences.append(chain)
+                chain = ""
+    if chain: # if the chain extends to the end
+        chained_sequences.append(chain)
+    return chained_sequences
+
+def calculate_from_console_input(max_shift):
+    sequence1 = get_user_input("Enter Sequence 1: ")
+    sequence2 = get_user_input("Enter Sequence 2: ")
+
+    chained_sequences = identify_chained_sequences(sequence1, sequence2)
+    print("Chained sequences without shift:", chained_sequences)
+
+    shift, matches, chain = calculate_best_shift(sequence1, sequence2, max_shift)
+    print(f"Best shift: {shift}, Matches: {matches}, Maximum Contiguous Chain: {chain}")
+
+def calculate_from_file_input(max_shift):
+    filename1 = get_user_input("Enter filename for Sequence 1: ")
+    filename2 = get_user_input("Enter filename for Sequence 2: ")
+    try:
+        sequence1 = load_sequence_from_file(filename1)
+        sequence2 = load_sequence_from_file(filename2)
+
+        shift, matches, chain = calculate_best_shift(sequence1, sequence2, max_shift)
+        print(f"Best shift: {shift}, Matches: {matches}, Maximum Contiguous Chain: {chain}")
+
+    except FileNotFoundError:
+        print("File not found!")
+    except ValueError:
+        print("File format is incorrect!")
+
 def calculate_matches(sequence1, sequence2, shift):
     # Shift sequence2
-    sequence2_shifted = sequence2[shift:] + " " * shift
+    sequence2_shifted = sequence2[-shift:] + sequence2[:-shift]
     matches = sum(1 for a, b in zip(sequence1, sequence2_shifted) if a == b)
     return matches
 
@@ -30,14 +64,16 @@ def calculate_max_contiguous_chain(sequence1, sequence2, shift):
 
 def calculate_best_shift(sequence1, sequence2, max_shift):
     best_shift = 0
-    best_matches = 0
-    best_chain = 0
+    best_matches = count_matches_without_shift(sequence1, sequence2)  # Start with no shift matches
+    best_chain = max(identify_chained_sequences(sequence1, sequence2), key=len, default="")
 
-    for shift in range(0, max_shift + 1):
-        current_matches = calculate_matches(sequence1, sequence2, shift)
-        current_chain = calculate_max_contiguous_chain(sequence1, sequence2, shift)
+    for shift in range(1, max_shift + 1):  # Start from 1 since we already considered shift = 0
+        sequence2_shifted = sequence2[-shift:] + sequence2[:-shift]
         
-        if current_matches > best_matches or (current_matches == best_matches and current_chain > best_chain):
+        current_matches = sum(1 for a, b in zip(sequence1, sequence2_shifted) if a == b)
+        current_chain = max(identify_chained_sequences(sequence1, sequence2_shifted), key=len, default="")
+        
+        if current_matches > best_matches or (current_matches == best_matches and len(current_chain) > len(best_chain)):
             best_shift = shift
             best_matches = current_matches
             best_chain = current_chain
@@ -50,19 +86,50 @@ def calculate_from_file_input(max_shift):
     try:
         sequence1 = load_sequence_from_file(filename1)
         sequence2 = load_sequence_from_file(filename2)
-        shift, matches, chain = calculate_best_shift(sequence1, sequence2, max_shift)
-        print(f"Best shift: {shift}, Matches: {matches}, Maximum Contiguous Chain: {chain}")
-    except FileNotFoundError:
-        print("File not found!")
-    except ValueError:
-        print("File format is incorrect!")
-        
 
+        # Matches without any shifts
+        matches = count_matches_without_shift(sequence1, sequence2)
+        print(f"Matches without any shifts: {matches}")
+
+        # Chained sequences without any shifts
+        chained_sequences = identify_chained_sequences(sequence1, sequence2)
+        print("Chained sequences without shift:", ', '.join(chained_sequences) if chained_sequences else "None")
+
+        # Best chain after performing shifts
+        shift, matches, chain = calculate_best_shift(sequence1, sequence2, max_shift)
+        print(f"\nAfter applying the best shift of {shift}:")
+        print(f"Matches: {matches}")
+        print(f"Maximum Contiguous Chain: {chain}")  # Print the chain
+
+    except FileNotFoundError:
+        print(f"Error: {filename1} or {filename2} was not found!")
+    except UnicodeDecodeError:
+        print("Error: Unable to decode file content!")
+    except ValueError:
+        print("Error: File format is incorrect!")
+
+
+def count_matches_without_shift(sequence1, sequence2):
+    return sum(1 for a, b in zip(sequence1, sequence2) if a == b)
+   
 def calculate_from_console_input(max_shift):
     sequence1 = get_user_input("Enter Sequence 1: ")
     sequence2 = get_user_input("Enter Sequence 2: ")
+
+    # Matches without any shifts
+    matches = count_matches_without_shift(sequence1, sequence2)
+    print(f"Matches without any shifts: {matches}")
+
+    # Chained sequences without any shifts
+    chained_sequences = identify_chained_sequences(sequence1, sequence2)
+    print("Chained sequences without shift:", ', '.join(chained_sequences) if chained_sequences else "None")
+
+    # Best chain after performing shifts
     shift, matches, chain = calculate_best_shift(sequence1, sequence2, max_shift)
-    print(f"Best shift: {shift}, Matches: {matches}, Maximum Contiguous Chain: {chain}")
+    print(f"\nAfter applying the best shift of {shift}:")
+    print(f"Matches: {matches}")
+    print(f"Maximum Contiguous Chain: {chain}")  # Print the chain
+
 
 def main_menu():
     print("1. Set Maximum Shift")
